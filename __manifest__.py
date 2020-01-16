5712e777180aa41676763286e7dc49996053dea8
Ethan Hildick <hildickethan@gmail.com>
2020-01-16 19:25:26 +0100

    crud with 2 models

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,6 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        # 'views/menus.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/models.py
+++ models/models.py
@@ -2,18 +2,18 @@
 
 from odoo import models, fields, api
 
-class worldhello(models.Model):
-    _name = 'worldhello.worldhello'
-    _description = 'default description'
+# class worldhello(models.Model):
+#     _name = 'worldhello.worldhello'
+#     _description = 'default description'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
 
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
 
 class questionmark(models.Model):
     _name = 'worldhello.questionmark'
@@ -21,3 +21,16 @@ class questionmark(models.Model):
 
     name = fields.Char(string="Title?", required=True)
     description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'worldhello.session'
+    _description = 'Session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(0,2), help="Duration in days")
+    seats = fields.Integer(string='Number of seats')
+    
+
+    
